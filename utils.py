fcb096aa703023d2c163888c59f621da4248b4ef
Abha2001 <56684260+Abha2001@users.noreply.github.com>
2019-12-26 00:17:32 +0530

    Add files via upload

--- utils.py
+++ utils.py
@@ -15,8 +15,26 @@ class Calendar(HTMLCalendar):
 			d+=f'<li>{event.EventName}<li>'
 
 		if day!=0:
-			return f'<td><span class='date'>{day}</span><ul>{d}</ul></td>'
+			return f"<td><span class='date'>{day}</span><ul>{d}</ul></td>"
 
 		return '<td></td>'
 
-	
+	def formatweek(self,theweek,events):
+		week=''
+		for d in theweek:
+			week+=self.formatday(d,events)
+		return f'<tr>{week}</tr>'
+
+	def formatmonth(self,withyear='True'):
+		events=Events.objects.filter(startTime__year=self.year,startTime__month=self.month)
+
+		cal=f"<table border='0' cell padding='0' cellspacing='0' class='calendar'>\n"
+		cal+=f'{self.formatmonthname(self.year,self.month,withyear=withyear)}\n'
+		cal+=f'<self.formatweekheader()>\n'
+		for week in self.monthdays2calendar(self.year,self.month):
+			cal+=f'{self.formatweek(week,events)}\n'
+		return cal
+
+
+
+
