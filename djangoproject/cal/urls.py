a662411775ecd37875bed85619f0fd6143aaa0a1
Abha2001 <56684260+Abha2001@users.noreply.github.com>
2020-01-06 08:38:24 +0530

    Update urls.py

--- djangoproject/cal/urls.py
+++ djangoproject/cal/urls.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url
 from .import views
 
+app_name='cal'
+
 urlpatterns=[
-	path('calendar/',views.CalendarView.as_view(),name='calendar'),
+	path('Calendar/',views.CalendarView.as_view(),name='calendar'),
+	path('event/new/',views.event,name='event_new'),
+	url(r'^event/edit/(?P<event_id>\d+)/',views.event,name='event_edit'),
 ]
